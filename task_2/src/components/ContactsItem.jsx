95c22ac800acb07888b6791c5d48877eef97eecd
Aidar Salimov <aidar97@bk.ru>
2022-11-19 16:47:38 +0300

    done

--- task_2/src/components/ContactsItem.jsx
+++ task_2/src/components/ContactsItem.jsx
@@ -1,7 +1,7 @@
 export default ({ img }) => {
   return (
     <li class="footer__item contacts__item">
-      <a href="!#" class="footer__link contacts__link">
+      <a href="#" class="footer__link contacts__link">
         {img}
       </a>
     </li>
